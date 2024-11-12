ae8cf4dcd83641168749286f3b0c5142b88b600d
k8abadzheva <make.me.loco@gmail.com>
2024-11-13 02:35:31 +0300

    feat: Add Help menu item to toolbar
    This commit adds a Help menu item to the toolbar:
    
    - Adds a new menu item with the id `helpFragment` to the `menu_toolbar.xml` file.
    - Sets the icon to `@android:drawable/ic_menu_help` and the title to "Help".
    - Sets `app:showAsAction` to `always` to display the item in the toolbar.
    - Adds a string resource for the menu item title with the name `help_menu`.
    - Inflates the toolbar menu in the `MainActivity`'s `onCreateOptionsMenu` method.
    - Handles the menu item selection in the `onOptionsItemSelected` method, navigating to the `helpFragment` destination using the Navigation component.

--- app/src/main/java/com/abadzheva/catchat/MainActivity.kt
+++ app/src/main/java/com/abadzheva/catchat/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.abadzheva.catchat
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.abadzheva.catchat.databinding.ActivityMainBinding
 
@@ -22,5 +29,21 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             insets
         }
         setSupportActionBar(binding.toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
