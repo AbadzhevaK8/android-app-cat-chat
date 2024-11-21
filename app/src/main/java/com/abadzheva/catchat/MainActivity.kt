c24544b12becad569fa44d798e65ea85920f25c1
k8abadzheva <make.me.loco@gmail.com>
2024-11-21 23:51:10 +0300

    feat: Implement Navigation Drawer
    This commit implements a navigation drawer for the app:
    
    - Replaces the bottom navigation with a navigation drawer.
    - Adds a header to the navigation drawer with an image of a kitten.
    - Updates the menu to include support items.
    - Refactors the layout to use a DrawerLayout.
    - Updates MainActivity to handle navigation using the drawer.
    - Adds strings for the support menu item and the kitten image description.

--- app/src/main/java/com/abadzheva/catchat/MainActivity.kt
+++ app/src/main/java/com/abadzheva/catchat/MainActivity.kt
@@ -3,12 +3,11 @@ package com.abadzheva.catchat
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -19,23 +18,20 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-        setSupportActionBar(binding.toolbar)
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
         val navHostFragment =
             supportFragmentManager
                 .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        val drawer = binding.drawerLayout
         val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawer)
         val appBarConfiguration = builder.build()
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
-        val bottomNavView = binding.bottomNav
-        bottomNavView.setupWithNavController(navController)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+        val navView = binding.navView
+        NavigationUI.setupWithNavController(navView, navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -44,14 +40,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
-        val navController = navHostFragment?.navController
-
-        return if (navController != null) {
-            item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
-        } else {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) ||
             super.onOptionsItemSelected(item)
-        }
     }
 }
