df6514bd1e99eb2f8cec61ae3c7662ad0e83816e
k8abadzheva <make.me.loco@gmail.com>
2024-11-23 02:38:59 +0300

    feat: Add custom navigation icon to Toolbar
    This commit introduces a custom navigation icon to the Toolbar for opening the navigation drawer:
    
    - Adds a custom vector drawable for the navigation icon (`baseline_lunch_dining_24`).
    - Disables the standard navigation drawer icon.
    - Sets the custom icon as the navigation icon for the Toolbar.
    - Adds a click listener to the Toolbar's navigation icon to open the navigation drawer.
    - Adds string resources for opening and closing the navigation drawer.
    - Uses an `ActionBarDrawerToggle` to handle the drawer's open and close state and synchronize it with the Toolbar.

--- app/src/main/java/com/abadzheva/catchat/MainActivity.kt
+++ app/src/main/java/com/abadzheva/catchat/MainActivity.kt
@@ -3,13 +3,13 @@ package com.abadzheva.catchat
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.abadzheva.catchat.databinding.ActivityMainBinding
 
@@ -32,11 +32,33 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration =
             AppBarConfiguration
                 .Builder(navController.graph)
-                .setOpenableLayout(binding.drawerLayout)
+                .setOpenableLayout(binding.drawerLayout) // Set the DrawerLayout with icon
                 .build()
 
         // Link Toolbar with NavController
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+//        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        // Here I'll try to add custom icon to the Toolbar
+        val toggle =
+            ActionBarDrawerToggle(
+                this,
+                binding.drawerLayout,
+                binding.toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close,
+            )
+        binding.drawerLayout.addDrawerListener(toggle)
+
+        // Disable the standard icon and install your own
+        toggle.isDrawerIndicatorEnabled = false
+        binding.toolbar.setNavigationIcon(R.drawable.baseline_lunch_dining_24)
+
+        // Add a handler to open DrawerLayout
+        binding.toolbar.setNavigationOnClickListener {
+            binding.drawerLayout.openDrawer(binding.navView)
+        }
+
+        toggle.syncState()
 
         // Set up NavigationView
         NavigationUI.setupWithNavController(binding.navView, navController)
